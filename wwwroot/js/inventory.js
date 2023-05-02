d0461909b316508196e3c62421ae0d51022ac823
zlamb1 <zlamb@my.wctc.edu>
2023-05-02 10:16:30 -0500

    added more sorting methods and a search box

--- wwwroot/js/inventory.js
+++ wwwroot/js/inventory.js
@@ -1,14 +1,16 @@
 $(async function()
 {
     let products = await loadProducts(); 
-    renderProducts((a, b) => 
+    let currentSort = (a, b) => 
     {
         if (a.unitsInStock == 0)
             return -1; 
         if (b.unitsInStock == 0)
             return 1;
         return (a.unitsInStock - a.reorderLevel) - (b.unitsInStock - b.reorderLevel); 
-    });
+    };
+
+    renderProducts();
 
     async function loadProducts()
     {
@@ -21,50 +23,99 @@ $(async function()
             }
         });
     }
-    
-    function renderProducts(sort)
+
+    function renderProducts()
     {
         $('#product_rows').html("");
+        let searchTerm = $('#product_search').val(); 
+        products.sort(currentSort);
 
-        products.sort(sort);
         for (var i = 0; i < products.length; i++)
         {
-            var category = ""; 
-            var css = "";
-
-            if (products[i].unitsInStock < products[i].reorderLevel)
+            let product = products[i]; 
+            if (searchTerm === undefined || product.productName.includes(searchTerm))
             {
-                css = "medium-stock";
+                let classes = "";
+                if (product.unitsInStock < product.reorderLevel)
+                {
+                    if (product.unitsInStock == 0)
+                        classes += "low-stock";
+                    else
+                        classes += "medium-stock";
+                }
+    
+                var row = `<tr data-id="${product.productId}" data-name="${product.productName}" data-price="${product.unitPrice}">
+                    <td class="${classes}">${product.productName}</td>
+                    <td class="text-right ${classes}">${product.category.categoryName}</td>
+                    <td class="text-right ${classes}">${product.unitsInStock}</td>
+                    <td class="text-right ${classes}">${product.reorderLevel}</td>
+                </tr>`;
+    
+                $('#product_rows').append(row);
             }
-
-            if (products[i].unitsInStock == 0)
-            {
-                css = "low-stock";
-            }
-
-            var row = `<tr data-id="${products[i].productId}" data-name="${products[i].productName}" data-price="${products[i].unitPrice}" style="user-select:none;">
-                <td class="${css}">${products[i].productName}</td>
-                <td class="text-right ${css}">${products[i].category.categoryName}</td>
-                <td class="text-right ${css}">${products[i].unitsInStock}</td>
-                <td class="text-right ${css}">${products[i].reorderLevel}</td>
-            </tr>`;
-
-            $('#product_rows').append(row);
         }
     }
 
+    $('#product_search').on('input', function()
+    {
+        renderProducts();
+    });
+
+    function invertAngle(element)
+    {
+        if (element.hasClass('fa-angle-down'))
+        {
+            element.removeClass('fa-angle-down').addClass('fa-angle-up');
+            return true;
+        } else
+        {
+            element.removeClass('fa-angle-up').addClass('fa-angle-down');
+            return false; 
+        }
+    }
+
+    $('#nameHeader').on('click', function()
+    {
+        let isUp = invertAngle($(this).children('i'));
+        if (isUp)
+            currentSort = (a, b) => -a.productName.localeCompare(b.productName);
+        else
+            currentSort = (a, b) => a.productName.localeCompare(b.productName); 
+
+        renderProducts(); 
+    });
+
+    $('#categoryHeader').on('click', function()
+    {
+        let isUp = invertAngle($(this).children('i'));
+        if (isUp)
+            currentSort = (a, b) => -a.category.categoryName.localeCompare(b.category.categoryName);
+        else
+            currentSort = (a, b) => a.category.categoryName.localeCompare(b.category.categoryName);
+
+        renderProducts(); 
+    });
+
     $('#unitsHeader').on('click', function()
     {
-        let icon = $(this).children('i');
-        if (icon.hasClass('fa-angle-down'))
-        {
-            icon.removeClass('fa-angle-down').addClass('fa-angle-up');
-            renderProducts((a, b) => b.unitsInStock - a.unitsInStock);
-        }
+        let isUp = invertAngle($(this).children('i')); 
+        if (isUp)
+            currentSort = (a, b) => b.unitsInStock - a.unitsInStock;
         else
-        {
-            icon.removeClass('fa-angle-up').addClass('fa-angle-down');
-            renderProducts((a, b) => a.unitsInStock - b.unitsInStock);
-        }
+            currentSort = (a, b) => a.unitsInStock - b.unitsInStock;
+
+        renderProducts(); 
     });
+
+    $('#reorderHeader').on('click', function()
+    {
+        let isUp = invertAngle($(this).children('i')); 
+        if (isUp)
+            currentSort = (a, b) => b.reorderLevel - a.reorderLevel;
+        else
+            currentSort = (a, b) => a.reorderLevel - b.reorderLevel; 
+
+        renderProducts();
+    });
+
 });
